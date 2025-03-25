87a02d4b1d8126957566a3a642766faa75d02d42
tellyxu <xutelly@gmail.com>
2025-03-25 13:14:11 -0400

    swap name

--- src/util/index.js
+++ src/util/index.js
@@ -2,6 +2,5 @@ const KEY = {
     count: "USE_COUNT"
 }
 
-
 export const addCount = newVal => window.localStorage.setItem(KEY.count, JSON.stringify(newVal))
 export const readCount = _=> JSON.parse(window.localStorage.getItem(KEY.count) || '0')
